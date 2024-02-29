226584d01bc0c2de4131aa79a96fd3c6f7b4efd6
Senninel <stefandavid18709@gmail.com>
2024-02-29 20:23:35 +0200

    Laborator 1

--- Laborator1/problema1.cpp
+++ Laborator1/problema1.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int Cvt(char s[])
 {
 	int numar = 0;
-	for (int i = 0;s[i] != NULL; i++)
+	for (int i = 0;s[i] != NULL && s[i] != '\n'; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9') numar = numar * 10 + (s[i] - '0');
 		else return 0;
@@ -25,7 +25,7 @@ int main()
 	else
 	{
 		char numar[200];
-		while(fgets(numar, 100, fp))
+		while (fgets(numar, 100, fp))
 		{
 			num = Cvt(numar);
 			suma += num;
